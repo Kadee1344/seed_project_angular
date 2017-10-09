b5f3ce687897eaec8c1f486a083107eb37a1886c
Kadee1344 <kadee1344@gmail.com>
2017-10-09 18:04:07 +0300

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
+});
+
+router.post('/', function (req, res, next) {
+  var message = req.body.message;
+  res.redirect('/message/' + message);
 });
 
 module.exports = router;
