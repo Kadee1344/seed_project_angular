e45b6abed0aff9664e22bfd22b577a5f2c5e704d
Kadee1344 <kadee1344@gmail.com>
2017-10-25 18:11:47 +0300

    Add routes: messages & users.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
+var messagesRoutes = require('./routes/messages');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
